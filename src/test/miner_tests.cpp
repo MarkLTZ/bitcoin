fdb7df325bff4572f97d0e14d5458310fc971793
LitecoinZ <team@litecoinz.info>
2020-11-15 21:25:51 +0100

    Update subsidy halving interval

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -385,8 +385,8 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
         ::ChainActive().SetTip(next);
     }
     BOOST_CHECK(pblocktemplate = AssemblerForTest(chainparams).CreateNewBlock(scriptPubKey));
-    // Extend to a 210000-long block chain.
-    while (::ChainActive().Tip()->nHeight < 210000) {
+    // Extend to a 840000-long block chain.
+    while (::ChainActive().Tip()->nHeight < 840000) {
         CBlockIndex* prev = ::ChainActive().Tip();
         CBlockIndex* next = new CBlockIndex();
         next->phashBlock = new uint256(InsecureRand256());
--- src/validation.cpp
+++ src/validation.cpp
@@ -1225,7 +1225,7 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
         return 0;
 
     CAmount nSubsidy = 50 * COIN;
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
+    // Subsidy is cut in half every 840,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
     return nSubsidy;
 }
