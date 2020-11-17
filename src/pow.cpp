d3b4f3e9f404fd124ae991143c18fd29923c1a6b
LitecoinZ <team@litecoinz.info>
2020-11-17 01:11:38 +0100

    CheckEquihashSolution clean-up

--- src/pow.cpp
+++ src/pow.cpp
@@ -182,8 +182,5 @@ bool CheckEquihashSolution(const CBlockHeader *pblock)
 
     bool isValid;
     EhIsValidSolution(n, k, state, pblock->nSolution, isValid);
-    if (!isValid)
-        return error("CheckEquihashSolution(): invalid solution n=%s,k=%s", n, k);
-
-    return true;
+    return isValid;
 }
